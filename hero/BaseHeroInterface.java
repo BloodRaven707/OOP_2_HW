5c10aa9ad0d73f46ee498eb350f024eb4f17a198
Dmitriy Dzhurabaev <blood.raven.707@gmail.com>
2023-03-14 01:55:01 +0300

    Initial commit

---
+++ hero/BaseHeroInterface.java
@@ -0,0 +1,21 @@
+package hero;
+
+
+public interface BaseHeroInterface {
+
+    // Действие - Шаг...
+    public default void step() {
+        System.out.println( getName() + " прощел еще немного... " );
+    }
+
+    // Действие - Шаг в направлении...
+    public default void step( String direction ) {
+        System.out.println( getName() + " прощел " + direction + " еще немного... " );
+    }
+    public default void stepUp()    { this.step( "вперед" ); }
+    public default void stepDown()  { this.step( "назад" ); }
+    public default void stepLeft()  { this.step( "влево" ); }
+    public default void stepRight() { this.step( "вправо" ); }
+
+    public default String getName()      { return ""; }
+}
